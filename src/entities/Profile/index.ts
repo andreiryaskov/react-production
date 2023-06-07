0320efe17877ada1299b18fb2be0571f750b0657
andreiryaskov <andreiryaskov@gmail.com>
2023-06-07 14:51:56 +0300

    added profile card, fetchProfileData

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -1,3 +1,12 @@
+export { getProfileIsLoading } from 'entities/Profile/model/selectors/getProfileLoading/getProfileLoading';
+
+export { ProfileCard } from './ui/ProfileCard/ProfileCard';
+
+export { getProfileData } from './model/selectors/getProfileData/getProfileData';
+export { getProfileError } from './model/selectors/getProfileError/getProfileError';
+
+export { fetchProfileData } from './model/services/fetchProfileData/fetchProfileData';
+
 export {
     Profile,
     ProfileSchema,
