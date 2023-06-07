a0814b23f9d30f9d022e2cc9e0f6fb9626740811
andreiryaskov <andreiryaskov@gmail.com>
2023-06-07 16:46:11 +0300

    fix componentRender

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,9 +1,9 @@
-import { ReactNode } from 'react';
-import { render } from '@testing-library/react';
-import { I18nextProvider } from 'react-i18next';
+import {ReactNode} from 'react';
+import {render} from '@testing-library/react';
+import {I18nextProvider} from 'react-i18next';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
-import { MemoryRouter } from 'react-router-dom';
-import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import {MemoryRouter} from 'react-router-dom';
+import {StateSchema, StoreProvider} from 'app/providers/StoreProvider';
 
 export interface componentRenderOptions {
     route?: string;
@@ -17,12 +17,12 @@ export function componentRender(component: ReactNode, options: componentRenderOp
     } = options;
 
     return render(
-        <StoreProvider initialState={initialState}>
-            <MemoryRouter initialEntries={[route]}>
+        <MemoryRouter initialEntries={[route]}>
+            <StoreProvider initialState={initialState}>
                 <I18nextProvider i18n={i18nForTests}>
                     {component}
                 </I18nextProvider>
-            </MemoryRouter>
-        </StoreProvider>,
+            </StoreProvider>
+        </MemoryRouter>,
     );
 }
